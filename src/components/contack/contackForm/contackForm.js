503a425e52132077ca4b919a46a13d620e938fea
黄星源 <12584315+yellow-star-source@user.noreply.gitee.com>
2024-09-25 15:38:45 +0800

    修改联系我简介

--- src/components/contack/contackForm/contackForm.js
+++ src/components/contack/contackForm/contackForm.js
@@ -5,10 +5,10 @@ export const ContackForm = () => {
     <div className='contack-form-content'>
 
 <form action="">
-  <input type="text" name="name" placeholder='请输入您的称呼'></input>
-  <input type="text" name="contack" placeholder='请输入微信号或邮箱'></input>
-  <textarea type="text" name="message" placeholder='请输入要对我说的话...'></textarea>
-  <button>发送</button>
+  <input type="text" name="name" placeholder='如果你对我感兴趣，通过左边的联系方式联系我吧！'></input>
+  <input type="text" name="contack" placeholder='希望我们的相遇能够为你的生活增添一点色彩！'></input>
+  <textarea type="text" name="message" placeholder='其他功能尽情期待~'></textarea>
+  <button>祝你生活，工作愉快！每天都有开心的时刻！</button>
 </form>
 
     </div>
